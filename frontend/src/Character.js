65a9bb67d45cac66575fdfa8f6d6a8fa5aa8a603
alexandreakdeniz <alexandre.akdeniz@free.fr>
2022-10-25 13:48:49 +0200

    Movements are ok

--- frontend/src/Character.js
+++ frontend/src/Character.js
@@ -43,16 +43,13 @@ export class Character
         else if (newPos.y > window.innerHeight - this.radius)
             newPos.y = window.innerHeight - this.radius;
 
-        console.log(`${newPos.x}px`, `${newPos.y}px`);
-
         this.entity.style.left = `${newPos.x}px`;
-        this.entity.style.top = `${newPos.x}px`;
+        this.entity.style.top = `${newPos.y}px`;
 
         return newPos;
     }
 
-    // deplace le joueur de moveX pixels sur en abscisee
-    // deplace le joueur de moveY pixels sur en ordonnee
+    // move: {x: 0, y: 0}
      move(move)
      {
          const pos = this.getPos();
@@ -64,20 +61,18 @@ export class Player extends Character
 {
     constructor(pos, radius, speed)
     {
-        console.log(pos);
-
         super(pos, radius, speed);
         this.entity.id = "player";
 
         document.addEventListener("keydown", (event) => {
             if (event.key === 'w')
-                this.move(0, -speed);
+                this.move({x: 0, y: -speed});
             if (event.key === 'd')
-                this.move(speed, 0);
+                this.move({x: speed, y: 0});
             if (event.key === 's')
-                this.move(0, speed);
+                this.move({x: 0, y: speed});
             if (event.key === 'a')
-                this.move(-speed, 0);
+                this.move({x: -speed, y: 0});
         })
         document.addEventListener("mousemove", (event) => this.turn(event.screenX, event.screenY))
     }
--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,10 +1,5 @@
 export function computeAngle(pos1, pos2)
 {
-
-
-    // pos1 = normalize(pos1);
-    // pos2 = normalize(pos2);
-
     const ref = {
         x: 0,
         y: -1
@@ -16,20 +11,8 @@ export function computeAngle(pos1, pos2)
         y: pos2.y - pos1.y
     })
 
-    console.log(dir);
-
-    let angle = Math.acos(dir.x * ref.x + dir.y * ref.y)  * 180 / Math.PI;
-
-    if (dir.x < 0)
-        angle = 360 - angle;
-
-    console.log(angle);
-
-    return angle;
-
-    // return Math.PI + Math.atan2(-pos2.x + pos1.x, pos2.y - pos1.y) * 180 / Math.PI;
-    // return Math.PI + Math.atan2(-dir.x + ref.x, dir.y - ref.y) * 180 / Math.PI;
-
+    const angle = Math.acos(dir.x * ref.x + dir.y * ref.y) * 180 / Math.PI;
+    return dir.x < 0 ? 360 - angle : angle;
 }
 
 function norm(v)
